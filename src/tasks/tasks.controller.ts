6c16a80c76a829656303ed2dfd200dcdfdfd5ed6
stubz151 <s215037588@mandela.ac.za>
2020-04-25 15:03:06 +0100

    initial setup

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { title } from 'process';
+import { stringify } from 'querystring';
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private tasksService : TasksService) {}
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.tasksService.getAllTasks();
+    }
+    
+    @Post()
+    createTask(
+        @Body('title')title: string,
+        @Body('description')description : string
+    )  : Task {
+        return this.tasksService.creatTask(title,description);
+    }
+}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import * as uuid from 'uuid/v1';
+
+@Injectable()
+export class TasksService {
+    private tasks: Task[] = [];
+
+    getAllTasks() : Task[]
+    {
+        return this.tasks;
+    }
+
+    creatTask(title: string, description: string) : Task {
+        const task: Task ={
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        };
+
+        this.tasks.push(task);
+        return task;
+    }
+
+}
